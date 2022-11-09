e414082bfa21cdd0e2576919ce9301a5cc009467
cesarcanoff <canoff.cesar@gmail.com>
2022-11-09 15:12:13 -0400

    Calculator Application Complete

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,24 +1,71 @@
-var operationArea = document.getElementById("full-operation");
-var resultArea = document.getElementById("full-result");
+var display = document.getElementById("full-result");
+var numberButtonList = document.querySelectorAll("[id*=key]");
+var historyButton = document.getElementById("history");
+var equalSymbol = document.getElementById("equals");
+var clearButton = document.getElementById("clear");
+var deleteButton = document.getElementById("backspace");
 
-function getElementOfHtml(element) {
-  insertTextOnOperationSection(element.innerHTML);
+var fullHistory = "";
+
+/*
+  TODO List
+
+  Saudações (z_z), provavelmente é o César do futuro que lerá isso,
+  Talvez esse projeto seja terminado totalmente daqui
+  2 dias, 2 meses ou 2 anos, mas o tempo é relativo senhores(a).
+  ----------------------------------------------------
+  - Refatorar as funções para arrow functions
+  - Estruturar melhor o HTML e organizar e otimizar o CSS.
+  - Diminuir a fonte quando ela ocupar 80% da tela
+  - Adicionar seção de histórico, para quando for
+    clicado o CE Button, a expressão seja inserida nessa seção.
+  - Colocar o ícone de BACKSPACE no botão.
+  - Encontrar uma forma de afastar os dígitos com um "", mesmo
+    podendo inserir números com decimais 
+*/
+
+function displayUpdate(text) {
+  display.textContent += `${text}`;
+  verifyIfGreaterThanEleven(text);
 }
 
-function insertTextOnOperationSection(text) {
-  let finalText = replaceMultiplicationSymbolToScreenInsert(text);
-  operationArea.innerHTML += finalText;
+function verifyIfGreaterThanEleven() {
+  if (display.textContent.length >= 8) {
+    display.style.fontSize = "2em";
+  }
 }
 
-function replaceMultiplicationSymbolToScreenInsert(symbol) {
-  return symbol === "X" ? symbol = "*" : symbol;
+function deleteDigitOfDisplay() {
+  let textDisplay = display.textContent;
+  let deleteDigit = textDisplay.substring(0, textDisplay.length - 1);
+  display.textContent = deleteDigit;
 }
 
-function getExpression() {
-  return operationArea.innerHTML;
+function calculateExpression(expression) {
+  let finalExpression = eval(expression);
+  display.textContent = "";
+  displayUpdate(finalExpression);
+  fullHistory = expression;
+  console.log(fullHistory);
 }
 
-function calculatingExpression() {
-  let expression = getExpression();
-  console.log(eval(expression));
+function clearDisplay() {
+  display.textContent = "";
 }
+
+equalSymbol.addEventListener("click", () =>
+  calculateExpression(display.textContent)
+);
+
+clearButton.addEventListener("click", () => clearDisplay());
+
+deleteButton.addEventListener("click", () => deleteDigitOfDisplay());
+
+historyButton.addEventListener(
+  "click",
+  () => (display.textContent = fullHistory)
+);
+
+numberButtonList.forEach((element) =>
+  element.addEventListener("click", () => displayUpdate(element.textContent))
+);
